68a62b25c26663a9973d3466d7e5cf5111dff33b
CPINEDA <cpinedae@miumg.edu.gt>
2019-05-14 17:55:57 -0600

    se agrego un mensaje

--- app/src/main/java/com/umg/cpineda/bluetooth/Voz.java
+++ app/src/main/java/com/umg/cpineda/bluetooth/Voz.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class Voz extends AppCompatActivity {
 
@@ -16,5 +17,7 @@ public class Voz extends AppCompatActivity {
     public void menu(View view){
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
+        System.out.println("Pasando al menu principal");
+        Toast.makeText(this, "Regresando al menú.", Toast.LENGTH_LONG).show();
     }
 }
